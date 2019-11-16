3990f242a21021aa612dbe7c12bd2532f9e2e839
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-11-16 17:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ let tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+let titleCased= (str) => str[0].toUpperCase() +
+str.slice(1).toLowerCase();
 
+let capitalizeWords = (str) => str.split('  ').map
+(titleCased).join('  ');
+  console.log(capitalizeWords(tutorials));
+
+<<<<<<< HEAD
 let map = tutorials.map(function titleCased(str){
   return str.toLowerCase().split(' ').map(function(chunk){
     return chunk.charAt(0).toUpperCase() + chunk.substring(1);
 }).join(' ');
 titleCased(tutorials);
 })
+=======
+>>>>>>> 6723c59de3ac2d3e8de8efd979f90468ae6ccd05
